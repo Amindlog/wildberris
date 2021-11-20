051ea28e62cbbf30f34575aff74e90600b6db1c3
Amind <amind@bk.ru>
2021-11-20 22:33:17 +0400

    edit error  localStorage

--- js/getGood.js
+++ js/getGood.js
@@ -1,18 +1,19 @@
 const getGoods = () => {
     const links = document.querySelectorAll('.navigation-link');
+
     const getData = () => {
         fetch('/db/db.json')
             .then((response) => response.json())
-            .then((date) => {
-                localStorage.setItem(JSON.stringify(data));
+            .then((data) => {
+                localStorage.setItem('data', JSON.stringify(data));
             });
-    }
+    };
+
     links.forEach((value) => {
         addEventListener('click', (e) => {
-            e.preventDefault()
+            e.preventDefault();
             getData();
-        })
+        });
     });
-
 }
 getGoods();
